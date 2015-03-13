7916ff179dede60d62743dd7ed0c289e859a6631
edison <yung751206@gmail.com>
2015-03-13 17:38:04 +0000

    complete map.c and wc.c

--- hw0/map.c
+++ hw0/map.c
@@ -5,7 +5,7 @@ int foo;            /* a staticly allocated variable */
 
 int recur(int i) {      /* a recursive function */
     int j = i;            /* a stack allocated variable within a recursive function */
-    printf("recur call %d: stack@ %lx\n", i, j); /* fix this so it prints the address of j */
+    printf("recur call %d: stack@ %lx\n", i, (long unsigned int)&j); /* fix this so it prints the address of j */
     if (i > 0) {
         return recur(i-1);
     }
@@ -18,10 +18,10 @@ int main (int argc, char *argv[]) {
     int i;            /* a stack allocated variable */
     char *buf1 = malloc(100); /* dynamically allocate some stuff */
     char *buf2 = malloc(100); /* and some more stuff */
-    printf("_main  @ %lx\n", main); /* fix to print address of main */
-    printf("recur @ %lx\n", recur); /* fix to print address of recur */
-    printf("_main stack: %lx\n", i); /* fix to get address of the stack variable */
-    printf("static data: %lx\n", stuff); /* fix to get address of a static variable */
+    printf("_main  @ %lx\n",(long unsigned int)&main); /* fix to print address of main */
+    printf("recur @ %lx\n", (long unsigned int)&recur); /* fix to print address of recur */
+    printf("_main stack: %lx\n", (long unsigned int)&i); /* fix to get address of the stack variable */
+    printf("static data: %lx\n", (long unsigned int)&stuff); /* fix to get address of a static variable */
     printf("Heap: malloc 1: %lx\n", (long unsigned int) buf1);
     printf("Heap: malloc 2: %lx\n", (long unsigned int) buf2);
     recur(3);
--- hw0/wc.c
+++ hw0/wc.c
@@ -1,9 +1,94 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
 void wc(FILE *ofile, FILE *infile, char *inname) {
+	int c;
+	int byte_count = 0;int word_count = 0;
+	int line_count = 0;int  word_flag = 0;
+	if(!infile){
+		char *line = NULL;
+		size_t len = 0;
+		ssize_t read;
+		while( (read = getline(&line,&len,stdin)) != -1){
+			size_t size = strlen(line);
+			int i;
+			for(i=0;i<size;i++){	
+				byte_count++;
+				char c = line[i];
+				if( (c == ' ' || c == '\n')  && word_flag){
+					word_count++;
+					word_flag = 0;
+				}
+				else if (c != ' ' || c != '\n'){
+					word_flag = 1;
+				}
+				if(c == '\n'){
+					line_count++;
+				}
+			}	
+		}
+		free(line);
+	}			
+	else if(infile){
+		while( (c=fgetc(infile)) != EOF ){
+			byte_count++;
+			if( (c == ' ' || c == '\n')  && word_flag){
+				word_count++;
+				word_flag = 0;
+			}
+			else if (c != ' ' || c != '\n'){
+				word_flag = 1;
+			}
+			if(c == '\n'){
+				line_count++;
+			}
+		}
+		fclose(infile);
+	}
+	if(ofile){
+		fprintf(ofile,"%d %d %d %s\n",line_count,word_count,byte_count,inname);	
+		fclose(ofile);
+	}
+	else{
+		if(inname)
+			printf("%d %d %d %s\n",line_count,word_count,byte_count,inname);
+		else	
+			printf("%d %d %d\n",line_count,word_count,byte_count);
+	}
 }
 
 int main (int argc, char *argv[]) {
-    return 0;
+	char* out_file_name = NULL;
+	char* inname = NULL;
+	if(argc == 1) wc(NULL,NULL,NULL);
+	else if(argc == 2){ 
+		inname = argv[1];
+		FILE *infile = fopen(inname,"r");
+		if(!infile){
+			perror("Input file opening failed");
+			exit(1);
+		}
+		wc(NULL,infile,inname);
+	}
+	else if(argc == 3){	
+		inname = argv[1];
+		FILE *infile = fopen(inname,"r");
+		if(!infile){
+			perror("Input file opening failed");
+			exit(1);
+		}
+		FILE *out_file = fopen(argv[2],"w");
+		if(!out_file){
+			perror("Output file opening failed");
+			exit(1);
+		}
+		wc(out_file,infile,inname);
+	}
+	else{
+		printf("arguments error\n");
+		exit(1);
+	}
+	return 0;
 }
